6c9ed88a96a266ad558e669e13eb8adc98a5817e
rekea <rekea@bb.com>
2021-07-06 11:40:01 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cors=require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -26,8 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/test', testRouter)
+app.use('/city', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.json({
+        message: 'fuck you all,what are you looking at'
+    })
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
