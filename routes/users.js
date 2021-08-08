1beed2cce9d5b1d4deb900cbc2f2d5a6e048255e
tommy <you@example.com>
2021-08-08 16:23:48 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -40,20 +40,22 @@ router.post('/', function (req, res, next) {
     data.ip = ip
     if (mobile === '967411328') {
         res.json({
-            fetchInterval: 1, // 分钟
-            clearInterval: 5760,// 分钟
-            noticeContent: {
-                strategy: 1,
-                isAd: true,
-                data: [
-                    {
-                        type: 'normal',
-                        desc: "<div style=\"font-size: 23px; text-align: center; color: red;\" ui-type=\"h5\" ui-comp=\"TextView\">🔥限时活动🔥</div>\n<div style=\"font-size: 17px;text-align: center;color: yellow;\" ui-type=\"h5\" ui-comp=\"LinkView\" ui-comp-event-props-url=\"https://dev.d3dsq7lelzkuwf.amplifyapp.com\" ui-comp-event-click=\"toUrl\">官方内部帐号开放购买，永久会员无需注册，只需100</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">AppleStore版本已经上线了</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">观影体验更佳，马上下载吧！</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">更有缓存功能想看就看！</div>\n<div ui-type=\"h5\" ui-comp=\"ButtonView\" style=\"white-space: normal; text-align: center;\" ui-comp-event-click=\"toBoBoAppleStoreApp\">AppleStore下载</div>",
-                        cover: 'https://assetscdn2.jable.tv/contents/videos_screenshots/16000/16035/320x180/1.jpg',
-                        link: 'https://jable.tv/videos/hnd-991/'
-                    }
-                ]
-            },
+            data: {
+                fetchInterval: 1, // 分钟
+                clearInterval: 5760,// 分钟
+                noticeContent: {
+                    strategy: 1,
+                    isAd: true,
+                    data: [
+                        {
+                            type: 'normal',
+                            desc: "<div style=\"font-size: 23px; text-align: center; color: red;\" ui-type=\"h5\" ui-comp=\"TextView\">🔥限时活动🔥</div>\n<div style=\"font-size: 17px;text-align: center;color: yellow;\" ui-type=\"h5\" ui-comp=\"LinkView\" ui-comp-event-props-url=\"https://dev.d3dsq7lelzkuwf.amplifyapp.com\" ui-comp-event-click=\"toUrl\">官方内部帐号开放购买，永久会员无需注册，只需100</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">AppleStore版本已经上线了</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">观影体验更佳，马上下载吧！</div>\n<div ui-type=\"h5\" ui-comp=\"TextView\" style=\"white-space: normal; text-align: center;\">更有缓存功能想看就看！</div>\n<div ui-type=\"h5\" ui-comp=\"ButtonView\" style=\"white-space: normal; text-align: center;\" ui-comp-event-click=\"toBoBoAppleStoreApp\">AppleStore下载</div>",
+                            cover: 'https://assetscdn2.jable.tv/contents/videos_screenshots/16000/16035/320x180/1.jpg',
+                            link: 'https://jable.tv/videos/hnd-991/'
+                        }
+                    ]
+                },
+            }
         })
     } else {
         res.json(jsonData)
