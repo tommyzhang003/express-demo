1783c4ed1bc309e24df96266c0755d5858fa140b
tommy <you@example.com>
2021-08-14 22:34:28 +0800

    update

--- routes/manager.js
+++ routes/manager.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var User = require('../models/user')
 var jsonData = require('./data')
+var axios = require('axios');
+var SocksProxyAgent = require('socks-proxy-agent');
 var router = express.Router();
+
 
 // 1 总是显示 非1有条件显示
 /* GET users listing. */
@@ -16,6 +19,42 @@ router.get('/', function (req, res, next) {
         }
     })
 });
+
+router.get('/proxy', function (err,res,next) {
+    const httpsAgent = new SocksProxyAgent({
+        host: '13.250.61.146',
+        port: 9090,
+        userId: 'gost',
+        protocol: 'socks5',
+        password: 'sdfrDrNUzd0usYySEeboM9uJEzHLMx0MzISFbjjq0Q23'
+    });
+    const url = 'http://fx-prod.bo.center/webapp/order/feed/user'
+    axios.get(url, {
+        httpsAgent,
+    }).then(response => {
+        res.send(response.data)
+        // console.log(response.data);
+    }).catch(error => {
+        res.json('error')
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            // console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+        // console.log(error.config);
+        // console.log(error);
+    })
+})
 
 
 module.exports = router;
